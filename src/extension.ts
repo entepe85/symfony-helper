2c376713467f26e4a72d68054b093b2696fbe0c3
Timur Morduhai <timur.morduhai@outlook.com>
2020-01-29 22:03:09 +0700

    move 'ComplexFileWatcher' class to its own file

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,6 @@ import {
     Range,
     Position,
     QuickPickItem,
-    RelativePattern,
-    Disposable,
-    WorkspaceFolder,
-    FileSystemWatcher,
 } from 'vscode';
 
 import {
@@ -28,6 +24,7 @@ import {
 
 import * as fs from 'fs';
 import { fileExists } from './utils';
+import ComplexFileWatcher from './ComplexFileWatcher';
 
 let client: LanguageClient;
 
@@ -330,60 +327,4 @@ export function deactivate() {
     }
 
     return client.stop();
-}
-
-class ComplexFileWatcher implements Disposable {
-    private watchers: FileSystemWatcher[] = [];
-    private client: LanguageClient;
-
-    constructor(client: LanguageClient) {
-        this.client = client;
-    }
-
-    public setFolders(folders: WorkspaceFolder[] | undefined) {
-        for (let w of this.watchers) {
-            w.dispose();
-        }
-
-        this.watchers.length = 0;
-
-        if (folders !== undefined) {
-            for (let f of folders) {
-                let w1 = workspace.createFileSystemWatcher(new RelativePattern(f, '{config,src,templates,app}/**/*.{php,twig,yaml,xml}'));
-                let w2 = workspace.createFileSystemWatcher(new RelativePattern(f, 'composer.lock'));
-                let watchers = [w1, w2];
-
-                for (let w of watchers) {
-                    w.onDidChange((e) => {
-                        this.client.sendRequest('documentChanged', {
-                            documentUri: e.toString(),
-                            action: 'createdOrChanged',
-                        });
-                    });
-
-                    w.onDidCreate((e) => {
-                        this.client.sendRequest('documentChanged', {
-                            documentUri: e.toString(),
-                            action: 'createdOrChanged',
-                        });
-                    });
-
-                    w.onDidDelete((e) => {
-                        this.client.sendRequest('documentChanged', {
-                            documentUri: e.toString(),
-                            action: 'deleted',
-                        });
-                    });
-
-                    this.watchers.push(w);
-                }
-            }
-        }
-    }
-
-    public dispose() {
-        for (let w of this.watchers) {
-            w.dispose();
-        }
-    }
 }
